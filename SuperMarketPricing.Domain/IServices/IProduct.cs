3b8779e1b47d4a2bde7e018fcb37c95bdc42b6a5
medalidenden <82745005+medalidenden@users.noreply.github.com>
2022-11-01 17:49:28 +0100

    Passage du Test à vert : TotalAmountForSimplePricing

---
+++ SuperMarketPricing.Domain/IServices/IProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMarketPricing.Domain.IServices
+{
+    public interface IProduct
+    {
+        string getName();
+        decimal getPrice();
+    }
+}
