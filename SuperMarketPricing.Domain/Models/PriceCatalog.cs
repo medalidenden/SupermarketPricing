a7768454936ff807c7c06331002e03608b940009
medalidenden <82745005+medalidenden@users.noreply.github.com>
2022-11-13 12:30:13 +0100

    update price catalog and price offer

--- SuperMarketPricing.Domain/Models/PriceCatalog.cs
+++ SuperMarketPricing.Domain/Models/PriceCatalog.cs
@@ -13,33 +13,27 @@ namespace SuperMarketPricing.Domain.Models
         }
         public decimal computeSpecialOffer(string name, int quantity)
         {
-            var OffersSpecial = priceOffers.FirstOrDefault(offering => offering.product.getName() == name && offering.category == Category.SpecialPrice);
-            if (OffersSpecial == null)
-                return computeFreelOffer(name, quantity);
-            else
-            {
-                return OffersSpecial.ComputeSpecialOffersPrice(quantity);
-            }
+            var OffersSpecial = priceOffers.FirstOrDefault(offering => offering.product.name == name && offering.category == Category.SpecialPrice);
+            
+            return OffersSpecial == null ? 
+                computeFreelOffer(name, quantity) : 
+                OffersSpecial.ComputeSpecialOffersPrice(quantity); 
         }
         public decimal computeFreelOffer(string name, int quantity)
         {
-            var OffersSpecialFree = priceOffers.FirstOrDefault(offering => offering.product.getName() == name && offering.category == Category.FreeProducts);
-            if (OffersSpecialFree == null)
-                return computeWeightedOffer(name, quantity);
-            else
-            {
-                return OffersSpecialFree.ComputeSpecialFreeOffersPrice(quantity);
-            }
+            var OffersSpecialFree = priceOffers.FirstOrDefault(offering => offering.product.name == name && offering.category == Category.FreeProducts);
+            
+            return OffersSpecialFree == null ? 
+                computeWeightedOffer(name, quantity)  : 
+                OffersSpecialFree.ComputeSpecialFreeOffersPrice(quantity); 
         }
         public decimal computeWeightedOffer(string name, int quantity)
         {
-            var WeightedOffer = priceOffers.FirstOrDefault(offering => offering.product.getName() == name && offering.category == Category.weightedProductsOffer);
-            if (WeightedOffer == null)
-                return priceOffers.FirstOrDefault(normalOffer => normalOffer.product.getName() == name).Price;
-            else
-            {
-                return WeightedOffer.ComputeWeightedOfferPrice(quantity);
-            }
+            var WeightedOffer = priceOffers.FirstOrDefault(offering => offering.product.name == name && offering.category == Category.weightedProductsOffer);
+            
+            return WeightedOffer == null ? 
+                priceOffers.FirstOrDefault(normalOffer => normalOffer.product.name == name).Price : 
+                WeightedOffer.ComputeWeightedOfferPrice(quantity); 
         }
     }
 }
